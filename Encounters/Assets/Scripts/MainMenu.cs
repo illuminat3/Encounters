0007b29b683e3113eb543d51ec1b20780c1acb73
Matthew <matthewrawson2@gmail.com>
2024-03-27 01:20:05 +0000

    Added Basic main menu

---
+++ Encounters/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
