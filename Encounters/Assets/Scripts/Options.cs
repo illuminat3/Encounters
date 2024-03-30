ec64a5f0b85558656b9d186a5834bc004d4f2e53
Matthew <matthewrawson2@gmail.com>
2024-03-30 16:53:31 +0000

    Added Back Button Logic

--- Encounters/Assets/Scripts/Options.cs
+++ Encounters/Assets/Scripts/Options.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using Data;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Back()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(Scenes.MainMenu);
     }
 }
