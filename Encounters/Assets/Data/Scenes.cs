814d8e1a0808997188fc2965b22ce1eafd514d3b
Matthew <matthewrawson2@gmail.com>
2024-03-30 16:05:45 +0000

    Added Scenes

---
+++ Encounters/Assets/Data/Scenes.cs
@@ -0,0 +1,9 @@
+﻿namespace Data
+{
+    public static class Scenes
+    {
+        public const string MainMenu = "Main Menu";
+        public const string LevelSelect = "Level Select";
+        public const string Options = "Options";
+    }
+}
